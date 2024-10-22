3be7e6ac56b184b5a9633f6f6a79dc070611e6bc
bob <45246438+bobmatyas@users.noreply.github.com>
2024-10-22 19:36:05 -0400

    update social image

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://wwww.bobmatyas.com',
+    site: 'https://www.bobmatyas.com',
     integrations: [mdx(), sitemap(), tailwind()],
 });
